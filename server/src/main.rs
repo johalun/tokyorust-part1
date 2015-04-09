a305406da66c735314a4ace1caed62eeb13a2c25
Johannes Lundberg <johannes@PC1-10-0013.local>
2015-04-09 17:49:19 +0900

    working client and server code

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,21 +1,45 @@
-extern crate ncurses;
+use std::net;
+use std::thread;
+use std::io::Write;
+use std::io::Read;
 
-use ncurses::*;
+fn main()
+{
+    println!("Welcome to Tokyo Rust Chat Server\n");
+        
+    let ip = net::Ipv4Addr::new(0, 0, 0, 0);
+    let port = 55555;
+    let addr = net::SocketAddrV4::new(ip, port);
 
-fn main() {
-    
-    /* Start ncurses. */
-    initscr();
+    let listener = net::TcpListener::bind(addr).unwrap();
 
-    /* Print to the back buffer. */
-    printw("Hello, world!");
+    println!("[Start listening for connections]");
+    for stream in listener.incoming() {
+        println!("[Got incoming connection, spawn thread]");
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move|| {
+                    handle_client(stream)
+                });
+            }
+            Err(e) => println!("[Connection failed: {}!]", e),
+        }
+    }    
+}
 
-    /* Update the screen. */
-    refresh();
-
-    /* Wait for a key press. */
-    getch();
-    
-    /* Terminate ncurses. */
-    endwin();
+fn handle_client(mut stream: net::TcpStream)
+{
+    let response = b"Welcome";
+    let mut message = [0; 128];
+ 
+    match stream.read(&mut message) {
+        Ok(size_read) => {
+            println!(">{} logs in.", std::str::from_utf8(&message[0..size_read]).unwrap());
+            match stream.write(response) {
+                Ok(size_sent) => println!("[Sent {} bytes reply to {}]", size_sent, std::str::from_utf8(&message[0..size_read]).unwrap()),
+                Err(e) => println!("[Failed sending message: {}!]", e),
+            }
+        },
+        Err(e) => println!("[Failed sending response: {}!]", e),
+    }
 }
